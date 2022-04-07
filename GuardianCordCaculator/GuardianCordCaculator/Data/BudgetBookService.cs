6efd838188143caa4bc118def22f80105cf033d2
jrallred125 <39539774+jrallred125@users.noreply.github.com>
2022-04-07 13:24:37 -0600

    Started a new project.

---
+++ GuardianCordCaculator/GuardianCordCaculator/Data/BudgetBookService.cs
@@ -0,0 +1,28 @@
+﻿namespace GuardianCordCaculator.Data
+{
+    public class BudgetBookService
+    {
+        private static List<List<CordModel>> Cords = new();
+
+        public async Task<bool>CaculateCords(CordModel cord)
+        {
+            int[] offset = new int[5] {2,13,29,45,55 };
+            for (int row = 0; row < offset.Length; row++)
+            {
+                Cords.Add(new List<CordModel>());
+                for (int col = 0; col < offset.Length; col++)
+                {
+                    Cords[row].Add( new(cord.XCord + offset[row], cord.ZCord + offset[col]));
+                }
+            }
+            return true;
+        }
+
+        public async Task<List<List<CordModel>>> GetSpawnCords()
+        {
+            return Cords;
+        }
+
+
+    }
+}
---
+++ GuardianCordCaculator/GuardianCordCaculator/Data/CordModel.cs
@@ -0,0 +1,19 @@
+﻿namespace GuardianCordCaculator.Data
+{
+    public class CordModel
+    {
+        public int XCord { get; set; }
+        public int ZCord { get; set; }
+
+        public CordModel(int xCord, int zCord)
+        {
+            XCord = xCord;
+            ZCord = zCord;  
+        }
+
+        public override string ToString()
+        {
+            return $"({XCord},{ZCord})";
+        }
+    }
+}
